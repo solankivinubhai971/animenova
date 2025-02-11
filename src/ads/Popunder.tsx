91daac6eef6044b3a3b90d62ab07932ba1119016
solankivinubhai971 <solankivinubhai971@gmail.com>
2025-02-11 12:33:25 +0530

    Add files via upload

---
+++ src/ads/Popunder.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from 'react';
+
+const Popunder = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = '//conductorhimselfwhipped.com/3c/b4/35/3cb435dc9a24129af480f97b1b0ddd30.js';
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return null;
+};
+
+export default Popunder;
